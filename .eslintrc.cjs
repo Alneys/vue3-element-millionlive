f7d58d1832c4010ba3b3f63c55c91072c2b939ca
Alneys <35741726+Alneys@users.noreply.github.com>
2022-08-26 12:59:35 +0800

    style: vue/multi-word-component-names: ignore index

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,5 +22,11 @@ module.exports = {
         endOfline: 'LF',
       },
     ],
+    'vue/multi-word-component-names': [
+      'error',
+      {
+        ignores: ['index'],
+      },
+    ],
   },
 };
