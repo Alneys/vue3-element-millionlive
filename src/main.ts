0dc040753d83ba7b712a63179286812f7c92ffe4
Alneys <35741726+Alneys@users.noreply.github.com>
2022-08-27 16:53:22 +0800

    feat: init layout

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,34 @@
+// npm init vue@latest
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 
-import './assets/main.css';
+// vue-router
+import router from './router';
 
+// Element Plus
+import ElementPlus from 'element-plus';
+// import 'element-plus/dist/index.css';
+import './assets/element-ml.scss'; // custom stylesheet for Element Plus
+import zhCn from 'element-plus/lib/locale/lang/zh-cn';
+// import en from 'element-plus/lib/locale/lang/en';
+// import ja from 'element-plus/lib/locale/lang/ja';
+
+// global stylesheet
+import './assets/global.scss';
+
+// ------------------------------------------------------------
+
+// npm init vue@latest
 const app = createApp(App);
 
 app.use(createPinia());
 
+// vue-router
+app.use(router);
+
+// Element Plus
+app.use(ElementPlus, { locale: zhCn, size: 'default', zIndex: 2000 });
+
+// npm init vue@latest
 app.mount('#app');
