54130ba0df82310d8fb36de277ae8ef5064ec748
Alneys <35741726+Alneys@users.noreply.github.com>
2022-08-29 11:59:13 +0800

    wip: add menu in LayoutAside

--- src/layout/LayoutAside.vue
+++ src/layout/LayoutAside.vue
@@ -1,17 +1,31 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import menuRoutes from '@/router/modules/menu';
+
+const route = useRoute();
+const activePath = computed(() => route.path);
+</script>
 
 <template>
   <div class="layout-aside">
     <el-scrollbar>
-      <el-menu router>
+      <el-menu router :default-active="activePath">
+        <el-menu-item
+          v-for="each in menuRoutes"
+          :key="each.name"
+          :index="each.path ? each.path : '/'"
+        >
+          <template #title>{{ each.meta.title }}</template>
+        </el-menu-item>
+        <el-menu-item index="/">
+          <template #title>/</template>
+        </el-menu-item>
         <el-menu-item index="/home">
-          <template #title>Home</template>
+          <template #title>/home</template>
         </el-menu-item>
         <el-menu-item index="/park">
           <template #title>Park</template>
-        </el-menu-item>
-        <el-menu-item index="/about">
-          <template #title>About</template>
         </el-menu-item>
         <el-menu-item index="/404">
           <template #title>404</template>
