729275aaba0a4ad6e3f489e0a4fcbf6c73360510
Alneys <35741726+Alneys@users.noreply.github.com>
2022-09-04 02:13:36 +0800

    fix(router): remove trailing slash

--- src/router/index.ts
+++ src/router/index.ts
@@ -15,4 +15,14 @@ const router = createRouter({
   routes: [...routes],
 });
 
+// beforeEach guard
+router.beforeEach((to) => {
+  // remove trailing slash
+  if (to.path.endsWith('/') && to.path !== '/') {
+    return to.path.replace(/\/+$/, '');
+  } else {
+    return true;
+  }
+});
+
 export default router;
