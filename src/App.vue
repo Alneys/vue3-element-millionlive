da31f68c94b265722f9f6e634f90fed9f87c6178
Alneys <35741726+Alneys@users.noreply.github.com>
2022-09-05 14:28:04 +0800

    feat: watch preferredLang to change router

--- src/App.vue
+++ src/App.vue
@@ -7,8 +7,11 @@ import type { Language } from 'element-plus/es/locale';
 import zhCn from 'element-plus/lib/locale/lang/zh-cn';
 import en from 'element-plus/lib/locale/lang/en';
 import ja from 'element-plus/lib/locale/lang/ja';
+import { useRoute, useRouter } from 'vue-router';
 
 const layoutStore = useLayoutStore();
+const route = useRoute();
+const router = useRouter();
 
 // i18n support
 const elLocaleList: Record<string, Language> = {
@@ -21,11 +24,27 @@ const elConfig = reactive({
   locale: elLocaleList[layoutStore.preferredLang] as Language,
 });
 
-// i18n support - watch layoutStore.preferredLang change
+// i18n support - watch route.params.preferredLang
+watch(
+  () => route.params.preferredLang as string | undefined,
+  (cur) => {
+    if (typeof cur === 'string') {
+      if (cur) layoutStore.preferredLang = cur;
+      else
+        layoutStore.preferredLang = import.meta.env.VITE_I18N_DEFAULT_LANGUAGE;
+    }
+  }
+);
+
+// i18n support - watch layoutStore.preferredLang
 watch(
   () => layoutStore.preferredLang,
   (cur) => {
     elConfig.locale = elLocaleList[cur]; // Element Plus i18n
+    const rawPath = route.params.preferredLang
+      ? route.path.replace(new RegExp(`^/${route.params.preferredLang}`), '')
+      : route.path;
+    router.replace(`/${cur}${rawPath}`);
   }
 );
 </script>
